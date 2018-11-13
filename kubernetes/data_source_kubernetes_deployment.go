78de53a3ef4c56717d6d7e8c28ee52848fe5b6d2
Iain Earl <synapse791@gmail.com>
2018-11-13 11:34:47 +0000

    Adding data source helper to generate data source from resource

--- kubernetes/data_source_kubernetes_deployment.go
+++ kubernetes/data_source_kubernetes_deployment.go
@@ -6,110 +6,17 @@ import (
 )
 
 func dataSourceKubernetesDeployment() *schema.Resource {
+	dsSchema := datasourceSchemaFromResourceSchema(resourceKubernetesDeployment().Schema)
+
+	addRequiredFieldsToSchema(dsSchema, "metadata")
+	addRequiredFieldsToSchema(dsSchema["metadata"].Elem.(*schema.Resource).Schema, "name")
+
+	addOptionalFieldsToSchema(dsSchema["metadata"].Elem.(*schema.Resource).Schema, "namespace")
+
 	return &schema.Resource{
 		Read: dataSourceKubernetesDeploymentRead,
 
-		Schema: map[string]*schema.Schema{
-			"metadata": namespacedMetadataSchema("deployment", false),
-			"spec": {
-				Type:        schema.TypeList,
-				Description: "Spec defines the specification of the desired behavior of the deployment. More info: http://releases.k8s.io/HEAD/docs/devel/api-conventions.md#spec-and-status",
-				MaxItems:    1,
-				Computed:    true,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"min_ready_seconds": {
-							Type:        schema.TypeInt,
-							Description: "Minimum number of seconds for which a newly created pod should be ready without any of its container crashing, for it to be considered available. Defaults to 0 (pod will be considered available as soon as it is ready)",
-							Computed:    true,
-						},
-						"paused": {
-							Type:        schema.TypeBool,
-							Description: "Indicates that the deployment is paused.",
-							Computed:    true,
-						},
-						"progress_deadline_seconds": {
-							Type:        schema.TypeInt,
-							Description: "The maximum time in seconds for a deployment to make progress before it is considered to be failed. The deployment controller will continue to process failed deployments and a condition with a ProgressDeadlineExceeded reason will be surfaced in the deployment status. Note that progress will not be estimated during the time a deployment is paused. Defaults to 600s.",
-							Computed:    true,
-						},
-						"replicas": {
-							Type:        schema.TypeInt,
-							Description: "The number of desired replicas. Defaults to 1. More info: http://kubernetes.io/docs/user-guide/replication-controller#what-is-a-replication-controller",
-							Computed:    true,
-						},
-						"revision_history_limit": {
-							Type:        schema.TypeInt,
-							Description: "The number of old ReplicaSets to retain to allow rollback. Defaults to 10.",
-							Computed:    true,
-						},
-						"selector": {
-							Type:        schema.TypeMap,
-							Description: "A label query over pods that should match the Replicas count. If Selector is empty, it is defaulted to the labels present on the Pod template. Label keys and values that must match in order to be controlled by this deployment, if empty defaulted to labels on Pod template. More info: http://kubernetes.io/docs/user-guide/labels#label-selectors",
-							Computed:    true,
-						},
-						"strategy": {
-							Type:        schema.TypeList,
-							Description: "Update strategy. One of RollingUpdate, Recreate. Defaults to RollingDate",
-							Computed:    true,
-							Elem: &schema.Resource{
-								Schema: map[string]*schema.Schema{
-									"type": {
-										Type:        schema.TypeString,
-										Description: "Update strategy",
-										Computed:    true,
-									},
-									"rolling_update": {
-										Type:        schema.TypeList,
-										Description: "rolling update",
-										Computed:    true,
-										Elem: &schema.Resource{
-											Schema: map[string]*schema.Schema{
-												"max_surge": {
-													Type:        schema.TypeString,
-													Description: "The maximum number of pods that can be scheduled above the desired number of pods. Value can be an absolute number (ex: 5) or a percentage of desired pods (ex: 10%). This can not be 0 if MaxUnavailable is 0. Absolute number is calculated from percentage by rounding up. Defaults to 25%. Example: when this is set to 30%, the new RC can be scaled up immediately when the rolling update starts, such that the total number of old and new pods do not exceed 130% of desired pods. Once old pods have been killed, new RC can be scaled up further, ensuring that total number of pods running at any time during the update is atmost 130% of desired pods.",
-													Computed:    true,
-												},
-												"max_unavailable": {
-													Type:        schema.TypeString,
-													Description: "The maximum number of pods that can be unavailable during the update. Value can be an absolute number (ex: 5) or a percentage of desired pods (ex: 10%). Absolute number is calculated from percentage by rounding down. This can not be 0 if MaxSurge is 0. Defaults to 25%. Example: when this is set to 30%, the old RC can be scaled down to 70% of desired pods immediately when the rolling update starts. Once new pods are ready, old RC can be scaled down further, followed by scaling up the new RC, ensuring that the total number of pods available at all times during the update is at least 70% of desired pods.",
-													Computed:    true,
-												},
-											},
-										},
-									},
-								},
-							},
-						},
-						"template": {
-							Type:        schema.TypeList,
-							Description: "Template describes the pods that will be created.",
-							Computed:    true,
-							Elem: &schema.Resource{
-								Schema: map[string]*schema.Schema{
-									"metadata": {
-										Type:        schema.TypeList,
-										Description: "Standard pod's metadata. More info: https://github.com/kubernetes/community/blob/master/contributors/devel/api-conventions.md#metadata",
-										Computed:    true,
-										Elem: &schema.Resource{
-											Schema: metadataFields("pod"),
-										},
-									},
-									"spec": {
-										Type:        schema.TypeList,
-										Description: "Template describes the pods that will be created.",
-										Computed:    true,
-										Elem: &schema.Resource{
-											Schema: podSpecFields(false),
-										},
-									},
-								},
-							},
-						},
-					},
-				},
-			},
-		},
+		Schema: dsSchema,
 	}
 }
 
